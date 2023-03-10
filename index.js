541b8c4d3b359f7482df93d9536f76471f795d04
Duneya <54691201+Duneyasaleh@users.noreply.github.com>
2023-03-10 11:38:44 +0100

    responsive

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express'
 
-const url ='https://whois.fdnd.nl/api/v1/squad/' 
+const url ='https://api.buurtcampus-oost.fdnd.nl/api/v1/stekjes' 
 const app = express()
 
 // Stel in hoe we express gebruiken
@@ -8,18 +8,10 @@ app.set('view engine', 'ejs')
 app.set('views', './views')
 app.use(express.static('public'))
 
-  // console.log(data)
-  //Js file:   // deep copy   // fuld way   // const filtered = JSON.parse(JSON.stringify(data))  
 
 
 // Maak een route voor de index
 app.get('/', (request, response) => {
-
-  let slug = request.query.squad || 'squad-b-2022'
-  let orderBy = request.query.orderBy || 'name'
-  let squadUrl = url + slug + '?orderBy=' + orderBy + '&direction=ASC'
-
-
   fetchJson(squadUrl).then((data) => {
   response.render('index', data)
   })
