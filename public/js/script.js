2994e6e1d1ea4358b516a5200d4396bc2f34814d
Duneya <54691201+Duneyasaleh@users.noreply.github.com>
2023-03-31 10:56:28 +0200

    versie 2

--- public/js/script.js
+++ public/js/script.js
@@ -1,41 +1,19 @@
-
-anychart.onDocumentReady(function() {
-    // create map
-    var map = anychart.map();
-  
-    // create data set
-    var dataSet = anychart.data.set(
-        [{"id":"NL.NH","value":0},
-              {"id":"NL.ZH","value":1},
-              {"id":"NL.LI","value":2},
-              {"id":"NL.GE","value":3},
-              {"id":"NL.NB","value":4},
-              {"id":"NL.OV","value":5},
-              {"id":"NL.UT","value":6},
-              {"id":"NL.DR","value":7},
-              {"id":"NL.FL","value":8},
-              {"id":"NL.GR","value":9},
-              {"id":"NL.ZE","value":10},
-              {"id":"NL.FR","value":11}]
-    );
-  
-    // create choropleth series
-    series = map.choropleth(dataSet);
-  
-    // set geoIdField to 'id', this field contains in geo data meta properties
-    series.geoIdField('id');
-  
-    // set map color settings
-    // series.colorScale(anychart.scales.linearColor('#deebf7', '#3182bd'));
-    // series.hovered().fill('#addd8e');
-  
-    // set geo data, you can find this map in our geo maps collection
-    // https://cdn.anychart.com/#maps-collection
-    map.geoData(anychart.maps['netherlands']);
-  
-    //set map container id (div)
-    map.container('container');
-  
-    //initiate map drawing
-    map.draw();
-  });
+window.addEventListener("load", () => {
+  // (A) GET HTML ELEMENTS
+  var filter = document.getElementById("the-filter"), // search box
+      plants = document.querySelectorAll("#the-plants"); // all plants items
+ 
+  // (B) ATTACH KEY UP plantsENER TO SEARCH BOX
+  filter.onkeyup = () => {
+    // (B1) GET CURRENT SEARCH TERM
+    let search = filter.value.toLowerCase();
+ 
+    // (B2) LOOP THROUGH plants ITEMS - ONLY SHOW THOSE THAT MATCH SEARCH
+    for (let i of plants) {
+      let item = i.innerHTML.toLowerCase();
+      if (item.indexOf(search) == -1) { i.classplants.add("hide"); }
+      else { i.class.plants.remove("hide"); }
+    }
+  };
+});
+ require('./bootstrap');
